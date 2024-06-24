ce4ebb42f8a89a83b6689d526c07e3336200da4b
kisai <sergiodnila@gmail.com>
2024-06-24 03:20:13 -0600

    adding initial test

--- spec/temperature_spread_spec.rb
+++ spec/temperature_spread_spec.rb
@@ -1,3 +1,17 @@
 # spec/temperature_spread_spec.rb
 # frozen_string_literal: true
 require 'spec_helper'
+
+require_relative '../bin/temperature_spread'
+
+RSpec.describe TemperatureSpread do
+  let(:test_file) { 'spec/fixtures/weather.dat' }
+
+  before do
+    @temperature_spread = TemperatureSpread.new(test_file)
+  end
+
+  it 'should return the day with the smallest temperature spread' do
+    expect(@temperature_spread.day_with_smallest_spread).to eq(10)
+  end
+end
